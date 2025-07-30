c77e21fe96ec826af849ca36fdcdadf71b4e7a17
Javier Ocampo <ferroxido@gmail.com>
2025-07-30 22:35:06 +0100

    chore: deploy to github pages

--- astro.config.mjs
+++ astro.config.mjs
@@ -5,6 +5,8 @@ import tailwindcss from '@tailwindcss/vite';
 
 // https://astro.build/config
 export default defineConfig({
+	site: 'https://javieroc.github.io',
+	base: 'portfolio-astro',
   vite: {
     plugins: [tailwindcss()]
   }
